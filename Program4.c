c4ac0f07b12c162e28d2a25e0e6ca78808c9acc9
Shubham997799 <136079809+Shubham997799@users.noreply.github.com>
2024-02-16 11:35:36 +0530

    Create Program4.c

---
+++ Program4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b,c;
+
+    printf("Enter a 1st Number :\n");
+    scanf("%d",&a);
+
+    printf("Enter A 2nd Number:\n");
+    scanf("%d",&b);
+
+    c=a+b;
+
+    printf("Addition is : %d\n",c);
+
+    return 0;
+}
